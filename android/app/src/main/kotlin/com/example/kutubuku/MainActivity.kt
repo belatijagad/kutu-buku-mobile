964a7bba4c98066d3cd2738045d04644e046a43f
Belati <belatijagadbintangsyuhada@gmail.com>
2023-12-04 13:21:18 +0700

    basic codebase

---
+++ android/app/src/main/kotlin/com/example/kutubuku/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kutubuku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        initialRoute: '/home',
+        routes: {
+          // '': (context) => Loading(),
+          // '/landing': (context) => Landing(),
+          // '/add_food': (context) => AddFood(),
+          // '/food_list': (context) => FoodList(),
+          // '/individual_food': (context) => IndividualFood(),
+          // '/login': (context) => Login(),
+          // '/register': (context) => Register(),
+        },
+      ),
+    );
