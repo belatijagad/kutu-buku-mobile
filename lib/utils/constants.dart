a3754233064cf738a1fe9820d05fc731b88be6ca
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-12-17 23:28:10 +0700

    Updated backed

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 
 class Constants {
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
+  static const String baseUrl = 'http://aldenluth.fi/api';
 
   static const String register = '$baseUrl/register/';
   static const String login = '$baseUrl/login/';
